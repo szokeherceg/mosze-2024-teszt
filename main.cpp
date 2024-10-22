ca48305297911d8e10a83377cb5d215a5ee5dfa7
szokeherceg <vasas.gergo2004@gmail.com>
2024-10-22 16:11:39 +0200

    hiba javítás

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];//N_ELEMENTS-nek kéne lennie
-    std::cout << '1-100 ertekek duplazasa' //nem macskaköröm között van,hiányzik az endline
-    for (int i = 0;)//NIncs befejezve
+    int *b = new int[N_ELEMENTS];//N_ELEMENTS-nek kéne lennie
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //nem macskaköröm között van,hiányzik az endline
+    for (int i = 0; i < N_ELEMENTS;i++)//NIncs befejezve
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)//hiányzik az i felsõ határértéke
+    for (int i = 0; i<N_ELEMENTS; i++)//hiányzik az i felsõ határértéke
     {
-        std::cout << "Ertek:" //hiányzik a pontosvesszõ és az endline
+        std::cout << "Ertek:" << std::endl; //hiányzik a pontosvesszõ és az endline
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //az átlag nem egész kell hogy legyen hanem double
-    for (int i = 0; i < N_ELEMENTS, i++)// az N_ELEMENTS és az i++ közé pontosvesszõ kell
+    double atlag; //az átlag nem egész kell hogy legyen hanem double
+    for (int i = 0; i < N_ELEMENTS; i++)// az N_ELEMENTS és az i++ közé pontosvesszõ kell
     {
-        atlag += b[i] //hiáyznik a pontosvesszõ
+        atlag += b[i]; //hiáyznik a pontosvesszõ
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
