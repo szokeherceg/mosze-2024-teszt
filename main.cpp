0363bb72d2c11ab189173c88394380c2c527043a
szokeherceg <vasas.gergo2004@gmail.com>
2024-10-22 16:07:29 +0200

    hiba kommentelés

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];//N_ELEMENTS-nek kéne lennie
+    std::cout << '1-100 ertekek duplazasa' //nem macskaköröm között van,hiányzik az endline
+    for (int i = 0;)//NIncs befejezve
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//hiányzik az i felsõ határértéke
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzik a pontosvesszõ és az endline
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //az átlag nem egész kell hogy legyen hanem double
+    for (int i = 0; i < N_ELEMENTS, i++)// az N_ELEMENTS és az i++ közé pontosvesszõ kell
     {
-        atlag += b[i]
+        atlag += b[i] //hiáyznik a pontosvesszõ
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
